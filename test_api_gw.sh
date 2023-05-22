964985e1cb2ec38795632bbea88f309ebefcc8e7
Cooper <cooperxmiller@gmail.com>
2023-05-22 17:57:38 -0500

    fix: clear something

--- test_api_gw.sh
+++ test_api_gw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 METHOD="POST"
-API_URL="https://c0rqms228h.execute-api.us-east-1.amazonaws.com"
+API_URL="https://x.execute-api.us-east-1.amazonaws.com"
 API_STAGE="v1"
 API_PATH="order"
 DATA='{"id":3,"amount":501}'
